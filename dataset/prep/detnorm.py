2ef8fa615f78012cb64c9d26ce04d71815fa68ed
thinhtq <Trinhthinh2171999@gmail.com>
2022-04-13 13:42:01 +0700

    initial

--- dataset/prep/detnorm.py
+++ dataset/prep/detnorm.py
@@ -30,26 +30,26 @@ class DetNorm:
     def _build(self, data: OrderedDict) -> OrderedDict:
         assert 'img' in data
         image: np.ndarray = data['img']
-        # h, w, c = image.shape
-        # if h > self.limit or w > self.limit:
-        #     scale = min([self.limit / w, self.limit / h])
-        #     new_h = math.floor(h * scale)
-        #     new_w = math.floor(w * scale)
-        #     data['img'] = cv2.resize(image,
-        #                              (new_w, new_h),
-        #                              interpolation=cv2.INTER_CUBIC)
-        #     data['threshMap'] = cv2.resize(data['threshMap'],
-        #                                    (new_w, new_h),
-        #                                    interpolation=cv2.INTER_CUBIC)
-        #     data['probMap'] = cv2.resize(data['probMap'],
-        #                                  (new_w, new_h),
-        #                                  interpolation=cv2.INTER_CUBIC)
-        #     data['threshMask'] = cv2.resize(data['threshMask'],
-        #                                     (new_w, new_h),
-        #                                     interpolation=cv2.INTER_CUBIC)
-        #     data['probMask'] = cv2.resize(data['probMask'],
-        #                                   (new_w, new_h),
-        #                                   interpolation=cv2.INTER_CUBIC)
+        h, w, c = image.shape
+        if h > self.limit or w > self.limit:
+            scale = min([self.limit / w, self.limit / h])
+            new_h = math.floor(h * scale)
+            new_w = math.floor(w * scale)
+            data['img'] = cv2.resize(image,
+                                     (new_w, new_h),
+                                     interpolation=cv2.INTER_CUBIC)
+            data['threshMap'] = cv2.resize(data['threshMap'],
+                                           (new_w, new_h),
+                                           interpolation=cv2.INTER_CUBIC)
+            data['probMap'] = cv2.resize(data['probMap'],
+                                         (new_w, new_h),
+                                         interpolation=cv2.INTER_CUBIC)
+            data['threshMask'] = cv2.resize(data['threshMask'],
+                                            (new_w, new_h),
+                                            interpolation=cv2.INTER_CUBIC)
+            data['probMask'] = cv2.resize(data['probMask'],
+                                          (new_w, new_h),
+                                          interpolation=cv2.INTER_CUBIC)
         image = (image.astype(np.float64) - self.mean) / 255.
         data['img'] = np.transpose(image, (2, 0, 1))
         return data
