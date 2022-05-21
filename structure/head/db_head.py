66ba4ec0085d52fef889676d9374d0989632fb86
Trinh_Thinh <Trinhthinh2171999@gmail.com>
2022-05-21 21:06:55 +0700

    initial

--- structure/head/db_head.py
+++ structure/head/db_head.py
@@ -1,3 +1,4 @@
+import torch
 from torch import nn, Tensor
 from collections import OrderedDict
 from typing import List
@@ -21,14 +22,29 @@ class DBHead(nn.Module):
             nn.Sigmoid()
         )
 
+        self.thresh: nn.Module = nn.Sequential(
+            nn.Conv2d(exp, exp_output, kernel_size=3, padding=1),
+            nn.BatchNorm2d(exp_output),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(exp_output, exp_output, kernel_size=2, stride=2),
+            nn.BatchNorm2d(exp_output),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(exp_output, 1, kernel_size=2, stride=2),
+            nn.AdaptiveAvgPool2d(1)
+        )
+
     def resize(self, x: Tensor, shape: List):
         return F.interpolate(x, shape, mode="bilinear", align_corners=True)
+
+    def binarization(self, probMap: Tensor, thresh: Tensor):
+        return torch.reciprocal(1. + torch.exp(-50 * (probMap - thresh)))
 
     def forward(self, x: Tensor, shape: List) -> OrderedDict:
         result: OrderedDict = OrderedDict()
         # calculate probability map
         probMap: Tensor = self.resize(self.prob(x), shape)
-        binaryMap: Tensor = probMap > self.thresh
+        thresh: Tensor = self.thresh(x)
+        binaryMap: Tensor = self.binarization(probMap, thresh)
         binaryMap = F.max_pool2d(binaryMap.float(), 9, 1, 4)
         result.update(probMap=probMap, binaryMap=binaryMap)
         return result
