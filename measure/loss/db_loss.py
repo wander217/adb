b8108446ad4cc2b13a4b2b5615b1990593b07824
Trinh_Thinh <Trinhthinh2171999@gmail.com>
2022-05-21 20:52:22 +0700

    initial

--- measure/loss/db_loss.py
+++ measure/loss/db_loss.py
@@ -19,7 +19,7 @@ class DBLoss(nn.Module):
         lossDict: OrderedDict = OrderedDict(probLoss=probDist)
         binaryDist: Tensor = self._binaryLoss(pred['binaryMap'],
                                               batch['binaryMap'],
-                                              batch['probMask'])
+                                              batch['probMap'][0])
         lossDict.update(binaryLoss=binaryDist)
         loss = probDist + binaryDist / 2
         return loss, lossDict
