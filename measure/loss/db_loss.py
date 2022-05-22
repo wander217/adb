070afb5a16eedbfd48b104e29e7365d6d658edc4
Trinh_Thinh <Trinhthinh2171999@gmail.com>
2022-05-22 11:16:25 +0700

    initial

--- measure/loss/db_loss.py
+++ measure/loss/db_loss.py
@@ -9,7 +9,7 @@ from torch import Tensor
 class DBLoss(nn.Module):
     def __init__(self, probLoss: Dict, binaryLoss: Dict):
         super().__init__()
-        self._probLoss: BceLoss = BceLoss(**probLoss)
+        self._probLoss: DiceLoss = DiceLoss(**probLoss)
         self._binaryLoss = BceLoss(**binaryLoss)
 
     def __call__(self, pred: OrderedDict, batch: OrderedDict) -> Tuple:
